bd501ac70f505d1169968a9dc9eafebf96f868d0
Matheus Almeida <mat.almeida@live.com>
2018-12-04 00:48:08 -0300

    feat: center style

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function Container({ title, center, dark, rounded, children }) {
+export default function Container({
+  title,
+  center,
+  dark,
+  rounded,
+  style,
+  children
+}) {
   const hasTitle = title ? "with-title" : "";
   const isCentered = center ? "is-center" : "";
   const isDark = dark ? "is-dark" : "";
   const isRounded = rounded ? "is-rounded" : "";
 
+  let daStyle = { ...style };
+
+  if (center) {
+    daStyle = {
+      ...daStyle,
+      textAlign: "center",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column"
+    };
+  }
+
   return (
     <section
       className={`container ${hasTitle} ${isCentered} ${isDark} ${isRounded}`}
+      style={daStyle}
     >
       <h2 className="title">{title}</h2>
       {children}
