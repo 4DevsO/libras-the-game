41eca705bc2db3ca150f0fa7d6db9f57f2e8abb2
Matheus Almeida <mat.almeida@live.com>
2018-12-04 00:48:18 -0300

    feat: home screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import Logo from "./components/Logo";
+import Container from "./components/Container";
+import Icon from "./components/Icon";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Logo />
-      </div>
+      <React.Fragment>
+        <Container title="O Jogo" center>
+          <Logo />
+        </Container>
+        <Container
+          center
+          style={{
+            position: "absolute",
+            bottom: "94px",
+            top: "146.5px",
+            right: 0,
+            left: 0
+          }}
+        >
+          <Icon size="extra-large" />
+          JOGAR
+        </Container>
+        <Container
+          title="Por"
+          center
+          style={{ position: "fixed", bottom: 0, right: 0, left: 0 }}
+        >
+          Equipe CdC
+        </Container>
+      </React.Fragment>
     );
   }
 }
